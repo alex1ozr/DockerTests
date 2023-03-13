2472a9274471eec23252d1f9381889a4f4b2608b
Ermilov Aleksei <alex.ozr@gmail.com>
2023-03-13 10:23:22 +0300

    TestContainers 3.0

--- tests/Api.IntegrationTests/TestApplication.cs
+++ tests/Api.IntegrationTests/TestApplication.cs
@@ -1,27 +1,22 @@
 ﻿using System.Data.Common;
 using DockerTestsSample.Common.Extensions;
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
-using DotNet.Testcontainers.Containers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Npgsql;
 using Respawn;
+using Testcontainers.PostgreSql;
 using Xunit;
 
 namespace DockerTestsSample.Api.IntegrationTests;
 
 public sealed class TestApplication : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
-    private readonly PostgreSqlTestcontainer _dbContainer =
-        new TestcontainersBuilder<PostgreSqlTestcontainer>()
-            .WithDatabase(new PostgreSqlTestcontainerConfiguration
-            {
-                Database = "TestDb",
-                Username = "user",
-                Password = "password"
-            }).Build();
-
+    private readonly PostgreSqlContainer _dbContainer = 
+        new PostgreSqlBuilder()
+        .WithDatabase("TestDb")
+        .WithUsername("user")
+        .WithPassword("password").Build();
+    
     private DbConnection? _dbConnection;
     private Respawner? _respawner;
     private HttpClient? _httpClient;
@@ -30,20 +25,16 @@ public sealed class TestApplication : WebApplicationFactory<IApiMarker>, IAsyncL
     private Respawner Respawner => _respawner.Required();
     public HttpClient HttpClient => _httpClient.Required();
     
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseSetting("ConnectionStrings:PopulationDbContext", _dbContainer.ConnectionString);
-    }
+    protected override void ConfigureWebHost(IWebHostBuilder builder) 
+        => builder.UseSetting("ConnectionStrings:PopulationDbContext", _dbContainer.GetConnectionString());
 
-    public async Task ResetDatabaseAsync()
-    {
-        await Respawner.ResetAsync(DbConnection);
-    }
+    public async Task ResetDatabaseAsync() 
+        => await Respawner.ResetAsync(DbConnection);
 
     public async Task InitializeAsync()
     {
         await _dbContainer.StartAsync();
-        _dbConnection = new NpgsqlConnection(_dbContainer.ConnectionString);
+        _dbConnection = new NpgsqlConnection(_dbContainer.GetConnectionString());
         _httpClient = CreateClient();
         await InitializeRespawner();
     }
@@ -58,8 +49,6 @@ public sealed class TestApplication : WebApplicationFactory<IApiMarker>, IAsyncL
         });
     }
 
-    public new async Task DisposeAsync()
-    {
-        await _dbContainer.StopAsync();
-    }
+    public new async Task DisposeAsync() 
+        => await _dbContainer.StopAsync();
 }
