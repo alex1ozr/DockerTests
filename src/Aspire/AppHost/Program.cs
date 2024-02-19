0b7745e90edd6be9bff7b43b7b157d88b1ef2b78
Ermilov Aleksei <alex.ozr@gmail.com>
2024-02-19 11:15:40 +0300

    -

--- src/Aspire/AppHost/Program.cs
+++ src/Aspire/AppHost/Program.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Api>("api");
+var dbName = "PopulationDb";
+var postgresDb = builder.AddPostgresContainer(name: "postgres", password: "mysecretpassword")
+    .WithAnnotation(new ContainerImageAnnotation { Image = "postgres", Tag = "15.3-alpine" })
+    // Set the name of the default database to auto-create on container startup.
+    .WithEnvironment("POSTGRES_DB", dbName)
+    // Add the default database to the application model so that it can be referenced by other resources.
+    .AddDatabase(dbName);
+
+builder.AddProject<Projects.Api>("api")
+    .WithReference(postgresDb);
 
 builder.Build().Run();
