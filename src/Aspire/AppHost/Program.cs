e67aa1e7c51a5bf27df47895ae76411ec7c7ba2d
Ermilov Aleksei <alex.ozr@gmail.com>
2024-02-22 12:09:48 +0300

    manifest.json

--- src/Aspire/AppHost/Program.cs
+++ src/Aspire/AppHost/Program.cs
@@ -4,7 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var dbName = "PopulationDb";
 var postgresDb = builder
-    .AddPostgresContainer(name: "postgres", port: 5432, password: "mysecretpassword")
+    .AddPostgresContainer(name: "PopulationDb", port: 5432, password: "mysecretpassword")
     .WithAnnotation(new ContainerImageAnnotation { Image = "postgres", Tag = "15.3-alpine" })
     // Set the name of the default database to auto-create on container startup.
     .WithEnvironment("POSTGRES_DB", dbName);
@@ -15,9 +15,6 @@ if (builder.Environment.IsDevelopment())
         // Mount the Postgres data directory into the container so that the database is persisted
         .WithVolumeMount("../../../data/postgres-mnt", "/var/lib/postgresql/data", VolumeMountType.Bind);
 }
-
-// Add the default database to the application model so that it can be referenced by other resources.
-postgresDb.AddDatabase(dbName);
 
 builder.AddProject<Projects.Api>("api")
     .WithReference(postgresDb);
